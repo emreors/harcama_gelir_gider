9a3a343f5450cd9e6b0fe7592188c140a3ce5ac9
emre.ors <emre.ors@bilgeadam.com>
2021-10-25 17:59:17 +0300

    Ara yüz düzenleme

---
+++ lib/kalem.dart
@@ -0,0 +1,12 @@
+class Kalem {
+  final String id;
+  final String baslik;
+  final double miktar;
+  final DateTime tarih;
+  Kalem({
+    required this.id,
+    required this.baslik,
+    required this.miktar,
+    required this.tarih,
+  });
+}
